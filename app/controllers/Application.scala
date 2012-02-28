a75ba3eee2ed1268cd4883fd3d065173619187a5
Kenichiro TANAKA <tanaka.kenichiro@gmail.com>
2012-02-28 23:32:59 +0900

    add guide5 http://playscalaja.appspot.com/documentation/0.9.1/guide5

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -13,12 +13,18 @@ import com.octo.captcha.service.image.ImageCaptchaService
 import javax.imageio.ImageIO
 import java.io.ByteArrayOutputStream
 import java.util.Locale
+import play.api.cache.Cache
+import java.util.UUID
 
 object Application extends Controller {
   val commentForm = Form(
     tuple(
       "author" -> nonEmptyText,
-      "content" -> nonEmptyText))
+      "content" -> nonEmptyText,
+      "code" -> nonEmptyText,
+      "randomID" -> nonEmptyText) verifying("Invalid code!", result => result match {
+        case (author, content, code, id) => captchaService.validateResponseForID(id, code)
+      }))
 
   def index = Action {
     val allPosts = Post.allWithAuthorAndComments
@@ -27,11 +33,10 @@ object Application extends Controller {
       allPosts.drop(1)))
   }
 
-  def show(id: Long) = Action {implicit request =>
-
+  def show(id: Long) = Action { implicit request =>
     Post.byIdWithAuthorAndComments(id).map {
       post =>
-        Ok(views.html.show(post, post._1.prevNext, commentForm))
+        Ok(views.html.show(post, post._1.prevNext, commentForm.fill("", "", "", UUID.randomUUID().toString())))
     } getOrElse {
       NotFound("No such Post")
     }
@@ -44,20 +49,19 @@ object Application extends Controller {
         BadRequest(views.html.show(post, post._1.prevNext, formWithErrors))
       },
       comment => {
-        Comment.create(Comment(NotAssigned, comment._1, comment._2, new Date, postId))
         Redirect(routes.Application.show(postId)).flashing("success" -> "Thanks for posting %s".format(comment._1))
       })
   }
-  
-  val captchaService :ImageCaptchaService = new DefaultManageableImageCaptchaService
-  
-  def captcha = Action{ implicit request => 
+
+  val captchaService: ImageCaptchaService = new DefaultManageableImageCaptchaService
+
+  def captcha(id: String) = Action { implicit request =>
+    // https://groups.google.com/forum/?fromgroups#!searchin/play-framework/2.0$20image/play-framework/5h5wh3eCiYo/1uTKQ2AQ3g4J
     // http://stackoverflow.com/questions/8305853/how-to-render-a-binary-with-play-2-0
     // http://d.hatena.ne.jp/kaiseh/20090502/1241286415
     val baos = new ByteArrayOutputStream
-    ImageIO.write(captchaService.getImageChallengeForID("", Locale.getDefault()), "jpg", baos);
+    ImageIO.write(captchaService.getImageChallengeForID(id, Locale.getDefault()), "jpg", baos);
     Ok(baos.toByteArray()).as("image/jpeg")
   }
-  
-  
+
 }
